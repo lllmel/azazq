5191a4649e3185502dbf7e4602987b0a0a5d6b60
Lllmel <59796476+lllmel@users.noreply.github.com>
2022-02-20 13:33:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+API_TOKEN = '5218149935:AAH8qNCC69ToT29CbWppROwM4lefow9WS7k' # токен от вашего бота в телеграме (взять тут t.me/botfather)
+number = '79049966033' # номер киви кошелька
+QIWI_SEC_TOKEN = 'eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6InY3NWU4MS0wMCIsInVzZXJfaWQiOiI3OTA0OTk2NjAzMyIsInNlY3JldCI6Ijk4M2U0ZWQ5MzRjYjIyOTkxNjJkOWMyZjQ4YjBjNzA3ZmU1NWViYzcxYzNjNDY1YjkyMTFmOTVlMzUzOWM1ZWYifX0=' # секретный ключ p2p https://p2p.qiwi.com
+admin = 1066406624 # id админа, узнать тут t.me/userinfobot
+admin_name = 'Azq' # username админа, без @
